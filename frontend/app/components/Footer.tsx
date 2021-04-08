e07b3dd65950c871af74d76a533ed234db41e38c
Tanaka Kent <tanaken0304.0123@gmail.com>
2021-04-08 17:05:12 +0900

    Add Footer Component

---
+++ frontend/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+    return (
+        <div className={styles.footter}>
+            © Copyright 2021 All rights reserved. Crafted by Kento Tanaka.
+        </div>
+    )
+}
+
+export default Footer;
