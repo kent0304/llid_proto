cd0d6304357e2c57cd8360a9ddd19bded1742127
Tanaka Kent <tanaken0304.0123@gmail.com>
2021-04-12 16:37:35 +0900

    Add Header component

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from "./Header.module.scss";
+import Link from 'next/link';
 
 const Header = () => {
     return (
         <div className={styles.header_container}>
-            <h1 className={styles.header_title}>Picture Description</h1>
+            <Link href="/">
+                <h1 className={styles.header_title}>Picture Description</h1>
+            </Link>
             <nav className={styles.header_list}>
                 <ul>
                     <li>
+                    <Link href="/">
                         Home
+                    </Link>
                     </li>
                     <li>
                         My Page
