602af6d08d22ed80e3c645d0e40fc2ea001d5907
Tanaka Kent <tanaken0304.0123@gmail.com>
2021-04-12 16:37:50 +0900

    Add submit button component

--- frontend/app/components/Submit.tsx
+++ frontend/app/components/Submit.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import styles from "./Submit.module.scss";
+import Link from 'next/link';
+
 
 type Submit = {
     content: string
+    link: string
 }
 
-const Submit = ( { content }: Submit ) => {
+const Submit = ( { content, link }: Submit ) => {
     return (
         <div>
             <button
             className={styles.button}
             >
-                {content}
+                <Link href={link}>
+                        {content}
+                </Link>
             </button>
         </div>
     )
