204088ccc290c5258d1289b272d670acd3b783ee
Tanaka Kent <tanaken0304.0123@gmail.com>
2021-04-13 15:02:01 +0900

    Add start button to top page

--- frontend/app/components/Top.tsx
+++ frontend/app/components/Top.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
+import Submit from "./Submit";
+import styles from "./Top.module.scss";
+
 
 const Top = () => {
     return (
         <div>
-            top画面やで
-            <br></br>
-            <Link href="question">
-                <a>問題はこちら</a>
-            </Link>
+            英文ライティング写真描画問題の練習webサイトです
+
+            <div className={styles.next_button}>
+                <Submit content="問題はこちら" link="question"/>
+            </div>
         
         </div>
     )
